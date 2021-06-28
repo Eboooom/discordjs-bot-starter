c035ee60c38d86dcf047423569184d804606e719
Eboooom <86570156+Eboooom@users.noreply.github.com>
2021-06-28 20:44:41 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -64,16 +64,16 @@ client.on('messageReactionAdd', (reaction, user) => {
   if (reaction.message.id === '738666523408990258') {
     switch (name) {
       case '🍎':
-        member.roles.add('738664659103776818');
+        member.roles.add('735100783883845652');
         break;
       case '🍌':
-        member.roles.add('738664632838782998');
+        member.roles.add('735100783883845652');
         break;
       case '🍇':
-        member.roles.add('738664618511171634');
+        member.roles.add('');
         break;
       case '🍑':
-        member.roles.add('738664590178779167');
+        member.roles.add('');
         break;
     }
   }
@@ -85,16 +85,16 @@ client.on('messageReactionRemove', (reaction, user) => {
   if (reaction.message.id === '738666523408990258') {
     switch (name) {
       case '🍎':
-        member.roles.remove('738664659103776818');
+        member.roles.remove('735100783883845652');
         break;
       case '🍌':
-        member.roles.remove('738664632838782998');
+        member.roles.remove('735100783883845652');
         break;
       case '🍇':
-        member.roles.remove('738664618511171634');
+        member.roles.remove('');
         break;
       case '🍑':
-        member.roles.remove('738664590178779167');
+        member.roles.remove('');
         break;
     }
   }
